0f19c6be19c77f727d4c0d0c52d04c5086795cc8
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-12-10 01:20:35 +0900

    refactor : request valid

--- src/main/java/com/gallendar/gradle/server/members/dto/LoginRequest.java
+++ src/main/java/com/gallendar/gradle/server/members/dto/LoginRequest.java
@@ -2,8 +2,12 @@ package com.gallendar.gradle.server.members.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 public class LoginRequest {
+    @NotBlank(message = "아이디를 입력하세요.")
     private String id;
+    @NotBlank(message = "비밀번호를 입력하세요.")
     private String password;
 }
