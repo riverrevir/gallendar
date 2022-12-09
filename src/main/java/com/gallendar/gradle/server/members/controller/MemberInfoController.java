e8c592d361e501e1cfc1632a5bb0c5b720639804
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-12-10 01:23:38 +0900

    refactor : member info

--- src/main/java/com/gallendar/gradle/server/members/controller/MemberInfoController.java
+++ src/main/java/com/gallendar/gradle/server/members/controller/MemberInfoController.java
@@ -2,23 +2,32 @@ package com.gallendar.gradle.server.members.controller;
 
 import com.gallendar.gradle.server.global.auth.jwt.JwtRequestFilter;
 import com.gallendar.gradle.server.members.dto.MemberInfoResponse;
+import com.gallendar.gradle.server.members.dto.MemberSearchResponse;
 import com.gallendar.gradle.server.members.dto.MemberTagStatusRequest;
 import com.gallendar.gradle.server.members.dto.MemberTagStatusResponse;
 import com.gallendar.gradle.server.members.service.MemberInfoService;
+import com.gallendar.gradle.server.members.service.MemberSearchService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
+@Validated
 @Slf4j
 public class MemberInfoController {
     private final MemberInfoService memberInfoService;
+    private final MemberSearchService memberSearchService;
 
     /**
      * 나의 정보 요청
@@ -29,7 +38,6 @@ public class MemberInfoController {
     @ApiOperation(value = "나의 정보 요청", notes = "아이디, 이메일 반환")
     @GetMapping
     public MemberInfoResponse getMyInfoById(@RequestHeader(value = JwtRequestFilter.HEADER_KEY) String token) {
-        log.info("마이페이지 조회 요청");
         return memberInfoService.myInfoGetById(token);
     }
 
@@ -44,7 +52,18 @@ public class MemberInfoController {
     @ApiOperation(value = "태그 로그 조회", notes = "누가 누구한테 공유 했는지와 상태를 반환")
     @GetMapping("/tag")
     public Page<MemberTagStatusResponse> getMySharedStatusById(@RequestHeader(value = JwtRequestFilter.HEADER_KEY) String token, MemberTagStatusRequest memberTagStatusRequest, Pageable pageable) {
-        log.info("태그 로그 조회 요청");
         return memberInfoService.mySharedStatusGetById(token, memberTagStatusRequest, pageable);
+    }
+
+    /**
+     * 유저 찾기(태그 추가할 때 사용)
+     *
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "유저 찾기", notes = "유저의 id 값으로 요청이 들어오면 해당 요청이 포함된 모든 결과를 리스트로 반환한다.")
+    @GetMapping
+    public List<MemberSearchResponse> searchMemberById(@RequestParam(value = "separatorKey") @NotBlank String id) {
+        return memberSearchService.MemberSearchById(id);
     }
 }
